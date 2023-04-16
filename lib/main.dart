d829f755f6a7796f4fc7acd8f25c7634910d9def
sayedanbia1 <sayedanbia5555@gmail.com>
2023-04-16 18:26:24 +0430

    seconde commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white30,
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
         title: Center(child: Text("I Am SeyyedAnbiaHosseini")),
